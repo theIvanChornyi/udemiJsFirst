213eceb6dd0abd940ccd7256177cd26112c9bc93
Ivan Chornyi <wanswans123@gmail.com>
2022-09-11 22:27:34 +0300

    complited first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',''),
+  lastFilm = prompt('Один из последних посмотреных фильмов',''),
+  latsFilmMark = prompt('На сколько оцение его?','');
+ 
+const personaMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personaMovieDB.movies[lastFilm] = +latsFilmMark;
+
+console.log(personaMovieDB);
